796b5a1e498ac410fdd5371782c732c875be2c6a
dm <d79123244656@yandex.ru>
2022-12-14 19:55:43 +0300

    lesson_9 Preporations for deployment

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
